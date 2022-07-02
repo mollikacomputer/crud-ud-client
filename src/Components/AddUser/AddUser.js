21780f194cff3a499c96ac7478864a852f9c25ec
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-02 21:57:33 +0600

    add some new feature

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import  {Link} from 'react-router-dom';
 const AddUser = () => {
+    const handleAddUser = event =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const user = {name, email};
+        console.log(user);
+        event.target.reset();
+        // post client side to serer side
+        
+    }
     return (
         <div>
             <h2> Add New User</h2>
@@ -9,6 +19,11 @@ const AddUser = () => {
                     <Link to='/' >Back to home</Link>
                 </li>
             </ul>
+            <form onSubmit={handleAddUser} >
+                <input type="text" name='name' placeholder='name' />
+                <input type="email" name='email' placeholder='email' />
+                <input type="submit" value="Add User" />
+            </form>
         </div>
     );
 };
